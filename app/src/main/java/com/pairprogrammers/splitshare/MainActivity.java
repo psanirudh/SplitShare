9c20ca7212fbd7e8baae554f23d6b3f5e9b77f25
DhanaDSP1120 <dhanadsp1120@gmail.com>
2024-01-03 23:30:33 +0530

    initial commit

---
+++ app/src/main/java/com/pairprogrammers/splitshare/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pairprogrammers.splitshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
