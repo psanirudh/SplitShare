136fde3fbf38679fc7fc9b419700cb48608be31e
ani <psanirudh@yahoo.com>
2026-07-22 01:26:11 +0530

    unwanted changes

--- QuickSplit/Backend/QuickSplit/QuickSplit/obj/Debug/net10.0/QuickSplit.AssemblyInfo.cs
+++ QuickSplit/Backend/QuickSplit/QuickSplit/obj/Debug/net10.0/QuickSplit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickSplit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f346a254c3d7d0286dbda902c4f7400185137c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickSplit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickSplit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
